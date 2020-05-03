40731e90b14b1d43dd85315cdd3aa5144b1f5dd4
Serena Hasler <srhasler@yahoo.com>
2020-05-03 15:52:26 -0500

    Get all emails and websites that end w/org. Use chaining to return only email addresses

--- index.mjs
+++ index.mjs
@@ -1,6 +1,11 @@
 import users from './data.mjs'
 
 // TODO: Get all emails that end with .org
-// const orgUsers = users.filter(user => user.email.endsWith('.org'))
-const orgUsers = users.filter(({ email }) => email.endsWith('.org'))
-console.log(orgUsers)
+/* const orgUsers = users.filter(({ email, website }) => email.endsWith('.org') || website.endsWith('.org'))
+
+const orgUserEmails = orgUsers.map(({ email }) => email) */
+
+// using destructuring here to look only at email & website
+const orgUserEmails = users.filter(({ email, website }) => email.endsWith('.org') || website.endsWith('.org')).map(({ email }) => email)
+
+console.log(orgUserEmails)
