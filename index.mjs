c06357dd815a250aba09d909fbd68d8e74dcac1d
Serena Hasler <srhasler@yahoo.com>
2020-05-03 13:14:43 -0500

    'Map' over 'groceryList' and 'transform' each item by adding 'from Schnucks'

--- index.mjs
+++ index.mjs
@@ -1,3 +1,14 @@
 import users from './data.mjs'
 
 console.log(users)
+
+const groceryList = ['Apples', 'Bananas', 'TP', 'Bars', 'Bacon', 'Eggs']
+
+// TODO 'map' over 'groceryList' and 'transform' each item by adding 'from Schnucks'
+// function (item) is a callback function
+const schnuckList = groceryList.map(function (item) {
+  item += ' from Schnucks' // item = item + 'from Schnucks'
+  return item
+})
+
+console.log(schnuckList, groceryList) // map creates a new, separate array while (generally) leaving the original array intact
