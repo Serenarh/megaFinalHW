fd0a37932b122a4b55440f6791d4e23996c352a1
Serena Hasler <srhasler@yahoo.com>
2020-05-03 15:29:41 -0500

    Get all emails that end with .org

--- index.mjs
+++ index.mjs
@@ -1,12 +1,6 @@
 import users from './data.mjs'
 
-// TODO: FILTER out all of the 'plurals'
-const groceryList = ['Apples', 'Bananas', 'TP', 'Bars', 'Bacon', 'Eggs']
-
-const plurals = groceryList.filter(function (item) {
-  // Does the item end with the letter 's'?
-  // RETURNS current element ('item') if callback RETURNS TRUE
-  return item.endsWith('s')
-})
-
-console.log(plurals, groceryList)
+// TODO: Get all emails that end with .org
+// const orgUsers = users.filter(user => user.email.endsWith('.org'))
+const orgUsers = users.filter(({ email }) => email.endsWith('.org'))
+console.log(orgUsers)
